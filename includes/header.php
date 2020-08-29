a2340c674e17c051fb6085666fe9c9fca89f3298
Iman Fattahi <imanfava@gmail.com>
2020-08-29 14:42:21 +0400

    Responsive support on devices

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php echo SITE_NAME; ?></title>
     <link rel="stylesheet" href="<?php url('/assets/package/bootstrap-4.5.2-dist/css/bootstrap.min.css'); ?>">
     <link rel="stylesheet" href="<?php url('/assets/css/main.css'); ?>">
